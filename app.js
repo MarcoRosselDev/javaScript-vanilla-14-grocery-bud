d91d4eaa73634df2ae2df6eca22093852a242edd
Marco Rossel <andresmarcorossel@gmail.com>
2022-08-07 10:20:08 -0400

    javaScript 3

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 
 const alert = document.querySelector('.alert');
 const form = document.querySelector('.grocery-form');
-const grocery = document.getElementById('.grocery');
+const grocery = document.getElementById('grocery');
 const submitBtn = document.querySelector('.submit-btn');
 const container = document.querySelector('.grocery-container');
 const list = document.querySelector('.grocery-list');
@@ -18,10 +18,15 @@ let editID = "";
 
 // ****** EVENT LISTENERS **********
 //submit form
-form.addEventListener('submit', )
+form.addEventListener('submit', addItem);
 
 // ****** FUNCTIONS **********
-
+function addItem(e){
+  e.preventDefault();
+  const value = grocery.value;
+  const id = new Date().getTime().toString();
+  console.log(id);
+}
 // ****** LOCAL STORAGE **********
 
 // ****** SETUP ITEMS **********
