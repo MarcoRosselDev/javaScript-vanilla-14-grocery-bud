512613514b82d785dc0b1b2a4c8d0ebd81bfd949
Marco Rossel <andresmarcorossel@gmail.com>
2022-08-08 02:14:28 -0400

    javaScript 5.4

--- app.js
+++ app.js
@@ -25,18 +25,34 @@ function addItem(e){
   e.preventDefault();
   const value = grocery.value;
   const id = new Date().getTime().toString();
-  if (value !== '' && editFlag === false) {
-    console.log('add item to the list');
-  } else if(value !== '' && editFlag === true) {
+  if (value && !editFlag) {
+
+    const element = document.createElement('article');
+      //add class
+      element.classList.add('.grocery-item');
+
+      //add id
+      const attr = document.createAttribute('data-id');
+      attr.value = id;
+      element.setAttributeNode(attr);
+      element.innerHTML = ``;
+
+  } else if(value && editFlag) {
     console.log('editing');
   } else {
-    
+    displayAlert('pleace enter value', 'danger')
   }
 }
 //display alert
 function displayAlert(text, action){
   alert.textContent = text;
   alert.classList.add(`alert-${action}`);
+
+  //remove alert
+  setTimeout(function(){
+    alert.textContent = '';
+    alert.classList.remove(`alert-${action}`);
+  },1000);
 }
 // ****** LOCAL STORAGE **********
 
