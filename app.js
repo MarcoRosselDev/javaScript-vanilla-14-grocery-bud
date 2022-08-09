5ae11cbb98b27a218b7bb4fec55020713974527e
Marco Rossel <andresmarcorossel@gmail.com>
2022-08-09 14:44:31 -0400

    javaScript 9.4

--- app.js
+++ app.js
@@ -162,17 +162,37 @@ function setBackToDefault(){
 // ****** LOCAL STORAGE **********
 function addToLocalStorage(id, value){
   const grocery = {id,value};
-  let items = localStorage.getItem('list')
-  ? JSON.parse(localStorage.getItem('list'))
-  : [];
-  console.log(items);
+  let items = getLocalStorage();
 
   items.push(grocery);
   localStorage.setItem('list', JSON.stringify(items));
 };
 
-function removeFromLocalStorage(id){};
-function editLocalStorage(id, value){};
+function removeFromLocalStorage(id){
+  let items = getLocalStorage();
+
+  items = items.filter(function(item){
+    if (item.id !== id) {
+      return item;
+    }
+  });
+  localStorage.setItem('list', JSON.stringify(items));
+};
+function editLocalStorage(id, value){
+  let items = getLocalStorage();
+  items = items.map(function(item){
+    if (item.id === id) {
+      item.value = value;
+    }
+    return item;
+  });
+  localStorage.setItem('list', JSON.stringify(items));
+};
+function getLocalStorage(){
+  localStorage.getItem('list')
+  ? JSON.parse(localStorage.getItem('list'))
+  : [];
+}
 // localStorage API
 // setItem
 // getItem
